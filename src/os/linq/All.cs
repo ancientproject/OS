6600b7e8d80cbb6b299a1eb33e3f598af4532fac
Yuuki Wesp <ls-micro@ya.ru>
2020-07-26 11:16:07 +0300

    add based linq implementation

---
+++ src/os/linq/All.cs
@@ -0,0 +1,20 @@
+﻿namespace IotHub.linq
+{
+    using System;
+
+    public static class AllEx
+    {
+        public static bool All<T>(this T[] items, Func<T, bool> predicate)
+        {
+            if (items.Length == 0)
+                return false;
+
+            var flag = true;
+
+            foreach (var item in items) 
+                flag &= predicate(item);
+
+            return flag;
+        }
+    }
+}
---
+++ src/os/linq/First.cs
@@ -0,0 +1,25 @@
+﻿namespace IotHub.linq
+{
+    using System;
+
+    public static class FirstEx
+    {
+        public static T First<T>(this T[] items)
+        {
+            if (items.Length == 0)
+                return default;
+            return items[0];
+        }
+    }
+
+    public static class SkipEx
+    {
+        public static T[] Skip<T>(this T[] items, int count)
+        {
+            var newItems = new T[items.Length - count];
+            for (var i = count; i != items.Length; i++) 
+                newItems[i - count] = items[i];
+            return newItems;
+        }
+    }
+}
